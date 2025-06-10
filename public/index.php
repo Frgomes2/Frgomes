00c6f2dbe93e535cad4f7f42760640226f4ba25e
Frgomes <flavio.raphael@msn.com>
2025-06-10 00:24:55 -0300

    composer

--- public/index.php
+++ public/index.php
@@ -1,19 +1,25 @@
 <?php
 
-// Path to the front controller (this file)
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
-
-// Ensure the current directory is pointing to the front controller's directory
-chdir(__DIR__);
+use CodeIgniter\CodeIgniter;
+use Config\Paths;
 
 // Load our paths config file
-require FCPATH . '../app/Config/Paths.php';
+require __DIR__ . '/../app/Config/Paths.php';
 
 // Create the paths instance
-$paths = new Config\Paths();
+$paths = new Paths();
 
 // Load the framework bootstrap file
 require rtrim($paths->systemDirectory, '\\/ ') . '/bootstrap.php';
 
-$app = Config\Services::codeigniter();
+/*
+ * ------------------------------------------------------
+ * Launch the application
+ * ------------------------------------------------------
+ * Now that everything is setup, it's time to actually fire
+ * up the engines and make this app do its thang.
+ */
+$app = new CodeIgniter(new \Config\App());
+$app->initialize();
 $app->run();
+
