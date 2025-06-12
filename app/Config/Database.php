d9a9f74b8c668375629c05ab37d0fef69a13ea55
Frgomes <flavio.raphael@msn.com>
2025-06-11 21:28:57 -0300

    config dominio

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -5,16 +5,16 @@ use CodeIgniter\Database\Config;
 class Database extends Config
 {
     public string $defaultGroup = 'default';
-    public array $default = [
-        'DSN'      => '',
-        'hostname' => getenv('PGHOST'),
-        'username' => getenv('PGUSER'),
-        'password' => getenv('PGPASSWORD'),
-        'database' => getenv('PGDATABASE'),
-        'DBDriver' => 'Postgre',
-        'port'     => getenv('PGPORT'),
-        'DBDebug'  => true,
-        'charset'  => 'utf8',
-        'DBCollat' => 'utf8_general_ci',
-    ];
+	public array $default = [
+		'DSN'      => '',
+		'hostname' => getenv('PGHOST'),
+		'username' => getenv('PGUSER'),
+		'password' => getenv('PGPASSWORD'),
+		'database' => getenv('PGDATABASE'),
+		'DBDriver' => 'Postgre',
+		'port'     => (int) getenv('PGPORT'), // <-- CORRIGIDO
+		'DBDebug'  => true,
+		'charset'  => 'utf8',
+		'DBCollat' => 'utf8_general_ci',
+	];
 }
