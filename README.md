9932a23455cda010d295fe821f3084008b8d8bdd
Frgomes <flavio.raphael@msn.com>
2025-06-11 21:45:09 -0300

    config dominio

--- README.md
+++
@@ -1,4 +0,0 @@
-# Projeto CodeIgniter com PostgreSQL no Railway
-
-Este projeto roda o CodeIgniter 4 usando PHP 8.2, Apache e PostgreSQL.  
-Hospedado no Railway via Docker.
--- app/Controllers/Home.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Controllers;
-class Home extends \CodeIgniter\Controller
-{
-	public function index()
-	{
-		return 'Bem-vindo ao CodeIgniter no Railway!';
-	}
-}
