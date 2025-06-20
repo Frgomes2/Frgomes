49e54c190fb97433d07803d22e3410d6964eff84
Frgomes <flavio.raphael@msn.com>
2025-06-19 22:39:23 -0300

    Atualizações no projeto

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://frgomes.com.br/'; 
+if (isset($_SERVER['HTTP_HOST'])) {
+    $config['base_url'] = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on' ? 'https' : 'http';
+    $config['base_url'] .= '://' . $_SERVER['HTTP_HOST'];
+    $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+} else {
+    // Fallback para linha de comando ou outros contextos
+    $config['base_url'] = 'https://frgomes.com.br/'; // Ou a URL de produção do seu Railway
+}
 
 /*
 |--------------------------------------------------------------------------
